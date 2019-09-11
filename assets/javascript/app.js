80f7c0f366d988c64448a22423dae33b15c5ea57
maurafortino <maurafortino@gmail.com>
2019-09-11 18:35:59 -0400

    finished game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -29,7 +29,7 @@ var trivia = [
 
         question: "What type of merchant is Team Avatar always running into?",
         options: ["squash", "cabbage", "watermelon", "cantelope"],
-        className: "quesetion-six"
+        className: "question-six"
     },
     {
 
@@ -43,17 +43,56 @@ var trivia = [
         className: "question-eight"
     }
 ]
-
+var clock = 120;
+var timer;
 var gameAnswers = ["100 years", "12", "cabbage", "Appa and Momo", "turned into the moon", "the blind bandit", "blood-bending", "fire"];
 var correctGuesses = [];
 var wrongGuesses = [];
-var isAnswerChosen;
 var startContainer = document.getElementById("start-container");
 var gameContainer = $("#game-container");
-var endContainer =$("#end-container");
+var endContainer = $("#end-container");
+// var currentTriviaIndex = 0;
+
+function timeConverter(t) {
+    var minutes = Math.floor(t / 60);
+    var seconds = t - (minutes * 60);
+
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    if (minutes === 0) {
+        minutes = "00";
+    }
+
+    else if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    return minutes + ":" + seconds;
+};
+
+function subtract() {
+    clock--
+    var setTime = timeConverter(clock);
+    $("#display").html(setTime)
+    if(clock === 0){
+        stopClock();
+        submit();
+    }
+};
+
+function stopClock() {
+    clearInterval(timer);
+    $("#display").html("00:00");
+
+};
 
 function enterGame() {
+    clearInterval(timer);
     gameContainer.empty();
+    wrongGuesses = [];
+    correctGuesses = [];
     userGuesses = [];
     startContainer.style.display = "none";
     gameContainer.css("display", "block");
@@ -68,18 +107,23 @@ function enterGame() {
         for (var j = 0; j < trivia[i].options.length; j++) {
             var optionsPar = $("<p>");
             optionsPar.text(trivia[i].options[j]);
+            optionsPar.addClass(trivia[i].className);
             optionsPar.attr("onclick", "addAnswer(this.textContent)");
             newDiv.addClass("trivia-div");
             newDiv.append(optionsPar);
             newDiv.css("margin-bottom", "10px");
-        }
+        };
         gameContainer.append(newDiv);
-    }
+    };
     var submitButton = $("<button>");
     submitButton.text("submit");
     submitButton.attr("onclick", "submit()");
     gameContainer.append(submitButton);
-}
+    
+    clock = 120;
+    timer = setInterval(subtract, 1000);
+
+};
 
 function addAnswer(parameter) {
 
@@ -89,15 +133,16 @@ function addAnswer(parameter) {
         wrongGuesses.push(parameter);
     }
 
-}
+};
 
 function submit() {
+    stopClock();
     endContainer.empty();
     gameContainer.css("display", "none");
     endContainer.css("display", "block");
     var newDiv = $("<div>");
-    newDiv.html("<p>Correct Guesses: " + correctGuesses.length +"</p>" 
-    + "<p> Wrong Guesses: " + wrongGuesses.length + "</p>");
+    newDiv.html("<p>Correct Guesses: " + correctGuesses.length + "</p>"
+        + "<p> Wrong Guesses: " + wrongGuesses.length + "</p>");
     endContainer.append(newDiv);
 
 
@@ -109,22 +154,21 @@ function submit() {
     }
     newDiv.append(ommitedDiv);
 
-    if (correctGuesses.length > wrongGuesses.length){
+    if (correctGuesses.length > wrongGuesses.length) {
         $("#game-over-text").text("Welcome to Team Avatar! We're happy to have you!")
         var winButton = $("<button>").text("Play again?");
         winButton.attr("onclick", "enterGame()");
         newDiv.append(winButton)
 
-    }else{
+    } else {
         $("#game-over-text").text("You're going to need a redemtion arc greater than Zuko's if you think you'll ever be on Team Avatar.")
         var loseButton = $("<button>").text("Redeem Yourself?");
         loseButton.attr("onclick", "enterGame()");
         newDiv.append(loseButton)
     }
 
+};
 
-
-}
 
 
 
